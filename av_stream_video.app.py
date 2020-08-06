bce3f4d32a05ec0bab04cab4d0de7858572e7d34
zer0 <osom8979@gmail.com>
2020-08-06 16:37:46 +0900

    Refactoring all scripts.

--- av_stream_video.app.py
+++ av_stream_video.app.py
@@ -3,34 +3,32 @@
 from functools import reduce
 
 import sys
-import multiprocessing as mp
+import time
+import argparse
+import psutil
+import numpy as np
+
 from multiprocessing.sharedctypes import Value
 from ctypes import c_bool
-import numpy as np
-from queue import Empty
+from multiprocessing import Process, Queue
+from queue import Full, Empty
 
-import av_stream_video_server
+import av_stream_video_server as vs
 
 
+LOGGING_PREFIX = '[av.stream_video] '
+LOGGING_SUFFIX = '\n'
+UNKNOWN_PID = 0
 DEFAULT_MAX_QUEUE_SIZE = 4
-DEFAULT_EXIT_TIMEOUT_SECONDS = 4.0
-
-src = ''
-video_index = 0
-frame_format = 'bgr24'
-options = {}
-reopen = False
-max_queue_size = DEFAULT_MAX_QUEUE_SIZE
-exit_timeout_seconds = DEFAULT_EXIT_TIMEOUT_SECONDS
 
 
 def print_out(message):
-    sys.stdout.write(message)
+    sys.stdout.write(LOGGING_PREFIX + message + LOGGING_SUFFIX)
     sys.stdout.flush()
 
 
 def print_error(message):
-    sys.stderr.write(message)
+    sys.stderr.write(LOGGING_PREFIX + message + LOGGING_SUFFIX)
     sys.stderr.flush()
 
 
@@ -58,128 +56,236 @@ def dict_to_str(items: dict, item_split=';', kv_split='='):
                   [str(k)+kv_split+str(v) for k, v in items.items()])
 
 
+class CreateProcessError(Exception):
+    pass
+
+
+class NullDataException(TypeError):
+    pass
+
+
+class StreamVideo:
+    """
+    """
+
+    def __init__(self,
+                 video_src='',
+                 video_index=0,
+                 frame_format=vs.DEFAULT_FRAME_FORMAT,
+                 options={},
+                 container_options={},
+                 stream_options=[],
+                 reconnect_sleep=vs.RECONNECT_SLEEP,
+                 max_queue_size=DEFAULT_MAX_QUEUE_SIZE,
+                 exit_timeout_seconds=vs.DEFAULT_EXIT_TIMEOUT_SECONDS,
+                 verbose=False):
+        self.video_src = video_src
+        self.video_index = video_index
+        self.frame_format = frame_format
+        self.options = options
+        self.container_options = container_options
+        self.stream_options = stream_options
+        self.reconnect_sleep = reconnect_sleep
+        self.max_queue_size = max_queue_size
+        self.exit_timeout_seconds = exit_timeout_seconds
+        self.verbose = verbose
+
+        self.last_image = None
+        self.exit_flag = Value(c_bool, False)
+        self.process: Process = None
+        self.queue: Queue = None
+        self.pid = UNKNOWN_PID
+
+    def on_set(self, key, val):
+        if key == 'video_src':
+            self.video_src = val
+        elif key == 'video_index':
+            self.video_index = int(val)
+        elif key == 'frame_format':
+            self.frame_format = val
+        elif key == 'options':
+            self.options = str_to_dict(str(val))
+        elif key == 'container_options':
+            self.container_options = str_to_dict(str(val))
+        elif key == 'stream_options':
+            self.stream_options = list(map(lambda x: x, str(val).split(',')))
+        elif key == 'max_queue_size':
+            self.max_queue_size = int(val)
+        elif key == 'exit_timeout_seconds':
+            self.exit_timeout_seconds = float(val)
+        elif key == 'verbose':
+            self.verbose = val.lower() in ['y', 'yes', 'true']
+
+    def on_get(self, key):
+        if key == 'video_src':
+            return self.video_src
+        elif key == 'video_index':
+            return str(self.video_index)
+        elif key == 'frame_format':
+            return self.frame_format
+        elif key == 'options':
+            return dict_to_str(self.options)
+        elif key == 'container_options':
+            return dict_to_str(self.container_options)
+        elif key == 'stream_options':
+            return ','.join(list(map(lambda x: str(x), self.stream_options)))
+        elif key == 'max_queue_size':
+            return str(self.max_queue_size)
+        elif key == 'exit_timeout_seconds':
+            return str(self.exit_timeout_seconds)
+        elif key == 'verbose':
+            return str(self.verbose)
+
+    def get_last_image(self):
+        try:
+            self.last_image = self.queue.get_nowait()
+        except Empty:
+            pass
+        return self.last_image
+
+    def _create_process_impl(self):
+        assert self.queue is None
+        assert self.process is None
+
+        self.queue = Queue(self.max_queue_size)
+        self.process = Process(target=vs.start_app,
+                               args=(self.queue, self.exit_flag, self.video_src,
+                                     self.video_index, self.frame_format, self.options,
+                                     self.container_options, self.stream_options,
+                                     self.reconnect_sleep, self.verbose,))
+
+        self.process.start()
+        if self.process.is_alive():
+            self.pid = self.process.pid
+            print_out(f'StreamVideo._create_process_impl() Server process PID: {self.pid}')
+            return True
+        else:
+            print_error(f'StreamVideo._create_process_impl() Server process is not alive.')
+            return False
+
+    def _create_process(self):
+        try:
+            return self._create_process_impl()
+        except Exception as e:
+            print_error(f'StreamVideo._create_process() Exception: {e}')
+            return False
+
+    def _close_process_impl(self):
+        if self.process is not None:
+            timeout = self.exit_timeout_seconds
+            if self.process.is_alive():
+                request_begin = time.time()
+                print_out(f'StreamVideo._close_process_impl() RequestExit(timeout={timeout}s)')
+                self.exit_flag = True
+                timeout = timeout - (time.time() - request_begin)
+                timeout = timeout if timeout >= 0.0 else 0.0
+
+            print_out(f'StreamVideo._close_process_impl() Join(timeout={timeout}s) the sub-process.')
+            self.process.join(timeout=timeout)
+
+            if self.process.is_alive():
+                print_error(f'StreamVideo._close_process_impl() Send a KILL signal to the server process.')
+                self.process.kill()
+
+        # A negative value -N indicates that the child was terminated by signal N.
+        print_out(f'StreamVideo._close_process_impl() The exit code of sub-process is {self.process.exitcode}.')
+
+        if self.queue is not None:
+            self.queue.close()
+            self.queue.cancel_join_thread()
+            self.queue = None
+
+        if self.process is not None:
+            self.process.close()
+            self.process = None
+
+        if self.pid >= 1:
+            if psutil.pid_exists(self.pid):
+                print_out(f'Force kill PID: {self.pid}')
+                psutil.Process(self.pid).kill()
+            self.pid = UNKNOWN_PID
+
+        assert self.queue is None
+        assert self.process is None
+        assert self.pid is UNKNOWN_PID
+        print_out(f'StreamVideo._close_process_impl() Done.')
+
+    def _close_process(self):
+        try:
+            self._close_process_impl()
+        except Exception as e:
+            print_error(f'StreamVideo._close_process() Exception: {e}')
+        finally:
+            self.queue = None
+            self.process = None
+            self.pid = UNKNOWN_PID
+
+    def create_process(self):
+        if self._create_process():
+            return True
+        self._close_process()
+        return False
+
+    def is_reopen(self):
+        if self.process is None:
+            return True
+        if not self.process.is_alive():
+            return True
+        return False
+
+    def reopen(self):
+        self._close_process()
+        if self._create_process():
+            print_out(f'Recreated Server process PID: {self.pid}')
+        else:
+            raise CreateProcessError
+
+    def on_init(self):
+        return self.create_process()
+
+    def on_valid(self):
+        return self.pid != UNKNOWN_PID
+
+    def on_run(self):
+        if self.is_reopen():
+            self.reopen()
+
+        frame = self.get_last_image()
+        if frame is None:
+            raise NullDataException
+
+        return {'frame': frame}
+
+    def on_destroy(self):
+        self._close_process()
+
+
+MAIN_HANDLER = StreamVideo()
+
+
 def on_set(key, val):
-    if key == 'src':
-        global src
-        src = val
-    elif key == 'video_index':
-        global video_index
-        video_index = int(val)
-    elif key == 'frame_format':
-        global frame_format
-        frame_format = val
-    elif key == 'options':
-        global options
-        options = str_to_dict(str(val))
-    elif key == 'reopen':
-        global reopen
-        reopen = bool(val)
-    elif key == 'max_queue_size':
-        global max_queue_size
-        max_queue_size = int(val)
-    elif key == 'exit_timeout_seconds':
-        global exit_timeout_seconds
-        exit_timeout_seconds = float(val)
+    MAIN_HANDLER.on_set(key, val)
 
 
 def on_get(key):
-    if key == 'src':
-        return src
-    elif key == 'video_index':
-        return str(video_index)
-    elif key == 'frame_format':
-        return frame_format
-    elif key == 'options':
-        return dict_to_str(options)
-    elif key == 'reopen':
-        return str(reopen)
-    elif key == 'max_queue_size':
-        return str(max_queue_size)
-    elif key == 'exit_timeout_seconds':
-        return str(exit_timeout_seconds)
-
-
-EMPTY_ARRAY = np.zeros((300, 300, 3), dtype=np.uint8)
-av_process: mp.Process
-av_queue: mp.Queue
-av_exit: Value
-av_last_data = EMPTY_ARRAY
-
-
-def run_process():
-    global max_queue_size
-    queue_size = max_queue_size if max_queue_size > 0 else DEFAULT_MAX_QUEUE_SIZE
-
-    global av_process, av_queue, av_exit
-    global src, video_index, frame_format, options, reopen
-
-    av_queue = mp.Queue(queue_size)
-    av_exit = Value(c_bool, False)
-    av_process = mp.Process(target=av_stream_video_server.on_runner,
-                            args=(av_queue, av_exit, src, video_index, frame_format, options, reopen,))
-    av_process.start()
-
-    print_out(f'av.stream_video process PID is {av_process.pid}.')
-    return av_process.is_alive()
-
-
-def close_process():
-    global av_exit
-    av_exit = True
-
-    global exit_timeout_seconds
-    timeout = exit_timeout_seconds if exit_timeout_seconds > 0.0 else DEFAULT_EXIT_TIMEOUT_SECONDS
-
-    global av_process
-    print_out(f'av.stream_video(pid={av_process.pid}) Join(timeout={timeout}s) ...')
-    av_process.join(timeout=timeout)
-
-    global av_queue
-    av_queue.close()
-    av_queue.cancel_join_thread()
-    av_queue = None
-
-    if av_process.is_alive():
-        print_error(f'av.stream_video(pid={av_process.pid}) Kill ...')
-        av_process.kill()
-
-    # A negative value -N indicates that the child was terminated by signal N.
-    print_out(f'av.stream_video(pid={av_process.pid}) Exit Code: {av_process.exitcode}')
-
-    av_process.close()
-    av_process = None
-
-
-def pop_array():
-    global av_queue, av_last_data
-    try:
-        data = av_queue.get_nowait()
-    except Empty:
-        data = None
-
-    if data is not None:
-        av_last_data = data
-
-    return av_last_data
+    return MAIN_HANDLER.on_get(key)
 
 
 def on_init():
-    return run_process()
+    return MAIN_HANDLER.on_init()
 
 
 def on_valid():
-    global av_process
-    return av_process.is_alive()
+    return MAIN_HANDLER.on_valid()
 
 
 def on_run():
-    return {'frame': pop_array()}
+    return MAIN_HANDLER.on_run()
 
 
 def on_destroy():
-    close_process()
+    return MAIN_HANDLER.on_destroy()
 
 
 if __name__ == '__main__':
-    # test_default()
     pass
